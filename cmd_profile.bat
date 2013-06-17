70d5e777d70158d87e12fb5c9b96002f4fd11db1
Geoff Stokes <geoff@geoffstokes.net>
2013-06-17 17:34:11 +1000

    Set and check for a variable at startup.
    Fixes a forkbomb experienced on some systems. Closes #1

--- cmd_profile.bat
+++ cmd_profile.bat
@@ -1,3 +1,7 @@
 @echo off
-doskey /macrofile=%USERPROFILE%/batfiles/doskey.mcr
-for /f "tokens=*" %%i in ('cscript //nologo %USERPROFILE%/batfiles/cmd_profile_getdocuments.vbs') do set Documents=%%i
+rem Make sure this only executes once per session
+IF NOT DEFINED cmdProfileExec (
+	set cmdProfileExec=true
+	doskey /macrofile=%USERPROFILE%\batfiles\doskey.mcr
+	for /f "tokens=*" %%i in ('cscript //nologo %USERPROFILE%\batfiles\cmd_profile_getdocuments.vbs') do set Documents=%%i
+)
